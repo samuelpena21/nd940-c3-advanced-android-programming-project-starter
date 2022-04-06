8625a570e56c46a3c1cc10a65fbf485424a17bb4
Samuel Peña <samuelpena098@gmail.com>
2022-04-05 20:04:20 -0400

    Open the downloaded files and Custom Url download

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,10 +1,15 @@
 package com.udacity
 
 import android.app.NotificationManager
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityDetailBinding
+
+private const val OPEN_DOWNLOAD = 3
 
 class DetailActivity : AppCompatActivity() {
 
@@ -20,13 +25,24 @@ class DetailActivity : AppCompatActivity() {
         val isSuccessful = intent.getBooleanExtra(MainActivity.IS_SUCCESSFUL_ARG, false)
 
         binding.contentDetail.fileNameValue.text = fileName
+
+        if (isSuccessful) {
+            binding.contentDetail.statusValue.text = getString(R.string.success)
+        } else {
+            binding.contentDetail.statusValue.text = getString(R.string.fail)
+            binding.contentDetail.statusValue.setTextColor(Color.RED)
+        }
+
         binding.contentDetail.statusValue.text =
             if (isSuccessful) getString(R.string.success) else getString(
 
                 R.string.fail
             )
-        binding.contentDetail.okBtn.setOnClickListener {
+        binding.contentDetail.backBtn.setOnClickListener {
             finish()
+        }
+        binding.contentDetail.viewFileBtn.setOnClickListener {
+            openDownloadedFile(isSuccessful)
         }
 
         cancelNotification()
@@ -38,5 +54,31 @@ class DetailActivity : AppCompatActivity() {
             NotificationManager::class.java
         ) as NotificationManager
         notificationManager.cancelAll()
+    }
+
+    private fun openDownloadedFile(isSuccessful: Boolean) {
+        if (!isSuccessful) return
+
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+        }
+
+        startActivityForResult(intent, OPEN_DOWNLOAD)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            OPEN_DOWNLOAD -> {
+                val path = data?.data?.path ?: ""
+                val uri = Uri.parse(path)
+                val intent = Intent()
+                intent.action = Intent.ACTION_VIEW
+                intent.setDataAndType(uri, "*/*")
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                startActivity(intent)
+            }
+        }
     }
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.udacity
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.annotation.SuppressLint
 import android.app.DownloadManager
 import android.app.DownloadManager.STATUS_SUCCESSFUL
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -11,14 +13,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Patterns
 import android.view.View
-import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityMainBinding
@@ -31,7 +36,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
-    private var selectedSource = DownloadSource.NO_SELECTION
 
     private lateinit var binding: ActivityMainBinding
 
@@ -46,7 +50,62 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         binding.contentMain.customButton.setOnClickListener {
-            download(selectedSource)
+            when (getRadioButtonSelectedSource()) {
+                DownloadSource.NO_SELECTION -> {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.please_select_one_option),
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                    binding.contentMain.customButton.stopLoading()
+                }
+                DownloadSource.ENTERED_TEXT -> {
+                    val textUrl = binding.contentMain.urlTv.text.toString()
+                    if (checkIfUrlIsValid(textUrl)) {
+                        download(textUrl)
+                    } else {
+                        Toast.makeText(this, "Please enter a valid Url", Toast.LENGTH_LONG).show()
+                        binding.contentMain.customButton.stopLoading()
+                    }
+                }
+                else -> {
+                    download(getRadioButtonSelectedSource().url)
+                }
+            }
+        }
+
+        binding.contentMain.radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when {
+                checkedId == binding.contentMain.enterUrl.id -> showTextAnimated()
+                binding.contentMain.enterUrl.visibility == View.VISIBLE -> hideTextAnimated()
+                else -> binding.contentMain.enterUrl.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun showTextAnimated() {
+        binding.contentMain.urlTv.apply {
+            alpha = 0f
+            visibility = View.VISIBLE
+            animate()
+                .setListener(null)
+                .alpha(1f)
+                .duration = 500L
+        }
+    }
+
+    private fun hideTextAnimated() {
+        binding.contentMain.urlTv.apply {
+            alpha = 1f
+            animate()
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        visibility = View.GONE
+                    }
+                })
+                .alpha(0f)
+                .duration = 500L
         }
     }
 
@@ -87,22 +146,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createNotification(notificationId: Int) {
-        val intent = Intent(applicationContext, DetailActivity::class.java)
+        val detailActivityIntent = Intent(applicationContext, DetailActivity::class.java)
 
         val isSuccess = isDownloadSuccessful()
-        intent.putExtra(FILE_NAME_ARG, selectedSource.name)
-        intent.putExtra(IS_SUCCESSFUL_ARG, isSuccess)
+        val selectedSourceName = getRadioButtonSelectedSource().title
+
+        detailActivityIntent.putExtra(FILE_NAME_ARG, selectedSourceName)
+        detailActivityIntent.putExtra(IS_SUCCESSFUL_ARG, isSuccess)
 
         pendingIntent = PendingIntent.getActivity(
             applicationContext,
             notificationId,
-            intent,
+            detailActivityIntent,
             PendingIntent.FLAG_UPDATE_CURRENT
         )
 
         action = NotificationCompat.Action(
             R.drawable.ic_assistant_black_24dp,
-            "Go to Download Details",
+            getString(R.string.go_to_download_details_action),
             pendingIntent
         )
 
@@ -111,11 +172,13 @@ class MainActivity : AppCompatActivity() {
             CHANNEL_ID
         )
             .setSmallIcon(R.drawable.ic_assistant_black_24dp)
-            .setContentTitle(
-                applicationContext
-                    .getString(R.string.notification_title)
+            .setContentTitle(selectedSourceName)
+            .setContentText(
+                resources.getString(
+                    R.string.notification_description,
+                    selectedSourceName
+                )
             )
-            .setContentText(applicationContext.getString(R.string.notification_description))
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .addAction(action)
@@ -138,18 +201,21 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    private fun download(downloadSource: DownloadSource) {
-        if (downloadSource == DownloadSource.NO_SELECTION) {
-            Toast.makeText(this, getString(R.string.please_select_one_option), Toast.LENGTH_SHORT)
-                .show()
+    private fun download(url: String) {
+        if (!hasPermissions()) {
+            requestPermissions()
             binding.contentMain.customButton.stopLoading()
             return
         }
 
         val request =
-            DownloadManager.Request(Uri.parse(downloadSource.url))
-                .setTitle(getString(R.string.app_name))
+            DownloadManager.Request(Uri.parse(url))
+                .setTitle(getRadioButtonSelectedSource().title)
                 .setDescription(getString(R.string.app_description))
+                .setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_DOWNLOADS,
+                    FILE_PATH + getRadioButtonSelectedSource().name + EXTENSION
+                )
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
@@ -159,25 +225,63 @@ class MainActivity : AppCompatActivity() {
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            val checked = view.isChecked
-            when (view.getId()) {
-                binding.contentMain.glide.id ->
-                    if (checked) {
-                        selectedSource = DownloadSource.GLIDE
-                    }
-                binding.contentMain.projectStarter.id ->
-                    if (checked) {
-                        selectedSource = DownloadSource.PROJECT_STARTER
-                    }
+    private fun getRadioButtonSelectedSource(): DownloadSource {
+        return when (binding.contentMain.radioGroup.checkedRadioButtonId) {
+            binding.contentMain.glide.id -> DownloadSource.GLIDE
+            binding.contentMain.projectStarter.id -> DownloadSource.PROJECT_STARTER
+            binding.contentMain.retrofit.id -> DownloadSource.RETROFIT
+            binding.contentMain.enterUrl.id -> DownloadSource.ENTERED_TEXT
 
-                binding.contentMain.retrofit.id ->
-                    if (checked) {
-                        selectedSource = DownloadSource.RETROFIT
+            else -> DownloadSource.NO_SELECTION
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            this,
+            WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty()) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.permission_granted),
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.permission_not_granted),
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
                     }
+                }
             }
         }
+    }
+
+    private fun checkIfUrlIsValid(url: String): Boolean {
+        return Patterns.WEB_URL.matcher(url).matches()
     }
 
     override fun onDestroy() {
@@ -188,17 +292,22 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val CHANNEL_ID = "channelId"
         private const val CHANNEL_NAME = "Download Channel"
+        private const val FILE_PATH = "/"
+        private const val EXTENSION = ".zip"
+        private const val PERMISSION_REQUEST_CODE = 1
         const val FILE_NAME_ARG = "file_name_arg"
         const val IS_SUCCESSFUL_ARG = "is_successful_arg"
     }
 
-    enum class DownloadSource(val url: String) {
-        NO_SELECTION(""),
-        GLIDE(url = "https://github.com/bumptech/glide"),
+    enum class DownloadSource(val url: String, val title: String) {
+        NO_SELECTION("", ""),
+        GLIDE(url = "https://github.com/bumptech/glide", title = "Glide"),
         PROJECT_STARTER(
             url = "https://github.com/udacity/nd940-c3-advanced-android" +
-                "-programming-project-starter"
+                "-programming-project-starter",
+            title = "Project Starter"
         ),
-        RETROFIT(url = "https://github.com/square/retrofit"),
+        RETROFIT(url = "https://github.com/square/retrofit", title = "Retrofit"),
+        ENTERED_TEXT(url = "", title = "Custom Download"),
     }
 }
